c963b962eb2fa3085e4471d8357f63bf288f30ac
Dan Levy <danlevy4@gmail.com>
2021-09-09 20:27:24 +0300

    feat.spinner

--- client/src/components/Plant.js
+++ client/src/components/Plant.js
@@ -3,11 +3,14 @@ import {Link} from 'react-router-dom'
 const Plant = ({plant}) => {
     return (
         <div className={'plant'}>
+
+            <Link to={`/plants/${plant.ID}`} >
+
             <h3>
                 {plant.Name}
                 {plant.Toxic? <FaSkull style={{color:'red'}}/> : <FaCheck style={{color:'green'}} />}
             </h3>
-            <Link to={`/plants/${plant.ID}`} >More details</Link>
+            </Link>
         </div>
     )
 }
--- client/src/components/PlantDetails.js
+++ client/src/components/PlantDetails.js
@@ -1,8 +1,10 @@
 import {useParams} from 'react-router-dom'
 import {useState,useEffect} from "react"
+import Loader from "react-loader-spinner";
 
 const PlantDetails = () => {
     const [plant,setPlant] = useState([]);
+    const [loading,setLoading] = useState(true);
     const {id} = useParams();
 
     useEffect(() => {
@@ -10,19 +12,27 @@ const PlantDetails = () => {
             const res = await fetch(`/api/plants/${id}`);
             const plant = await res.json();  
             setPlant(plant);
+            setLoading(false);
           }
           getPlantDetails();
     },[id]);
 
-    return (
-        <div>
-            <h1>{plant.Name}</h1>
-            <img src={plant.img} alt=""/>
-            <h3>{plant.additionalNames}</h3>
-            <h3>{plant.scienceName}</h3>
-            <h3>{plant.family}</h3>
-        </div>
+    const Details = ()=>{
+        return (
+            <div className="container">
+                <h1>{plant.Name}</h1>
+                <img src={plant.img} alt=""/>
+                <h3>{plant.additionalNames}</h3>
+                <h3>{plant.scienceName}</h3>
+                <h3>{plant.family}</h3>
+            </div>
+        )
+    }
+    return (loading ? <Loader type="BallTriangle" color="#00BFFF" height={80} width={80}/> : <Details/> 
+
     )
+
+
 }
 
 export default PlantDetails
